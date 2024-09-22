5936fa1c2668388aa068d963870ffe96c16a81bf
jieun <sdsm0301@gmail.com>
2024-09-22 19:33:14 +0900

    /

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ document.addEventListener("DOMContentLoaded", function() {
  function showCharacter(character) {
   let characterInfo = {
     yohei: {
-      name: "이치노세 요헤이",
-      imageLeft: "https://via.placeholder.com/400x700?text=image", //전신
-      imageRight: "https://via.placeholder.com/700x300?text=image+Side",  //두상
+      name: "<span style='color:#F7CF00'>이치노세 요헤이</span>",
+      imageLeft: "https://via.placeholder.com/400x700?text=400x700", //전신
+      imageRight: "https://via.placeholder.com/700x300?text=700x300",  //두상
       description: `
         3학년 1반. 히가시야마고교의 학생회장.<br>
         검도부 출신으로 목소리가 크고 호탕한 성격.<br>
@@ -17,57 +17,57 @@ document.addEventListener("DOMContentLoaded", function() {
       `
     },
     yoshino: {
-      name: "요시노 린",
-      imageLeft: "https://via.placeholder.com/400x700?text=image", //전신
-      imageRight: "https://via.placeholder.com/700x300?text=image+Side",  //두상
+      name: "<span style='color:#658A5A'>요시노 린</span>",
+      imageLeft: "https://via.placeholder.com/400x700?text=400x700", //전신
+      imageRight: "https://via.placeholder.com/700x300?text=700x300",  //두상
       description: `
         내용
       `
     },
     chiharu: {
-      name: "사카가미 치하루",
-      imageLeft: "https://via.placeholder.com/400x700?text=image", //전신
-      imageRight: "https://via.placeholder.com/700x300?text=image+Side",  //두상
+      name: "<span style='color:#C4C2D3'>사카가미 치하루</span>",
+      imageLeft: "https://via.placeholder.com/400x700?text=400x700", //전신
+      imageRight: "https://via.placeholder.com/700x300?text=700x300",  //두상
       description: `
         내용
       `
     },
     shigeto: {
-      name: "시게토 미사오",
-      imageLeft: "https://via.placeholder.com/400x700?text=image", //전신
-      imageRight: "https://via.placeholder.com/700x300?text=image+Side",  //두상
+      name: "<span style='color:#32465C'>시게토 미사오</span>",
+      imageLeft: "https://via.placeholder.com/400x700?text=400x700", //전신
+      imageRight: "https://via.placeholder.com/700x300?text=700x300",  //두상
       description: `
         내용
       `
     },
     moe: {
-      name: "혼다 모에",
-      imageLeft: "https://via.placeholder.com/400x700?text=image", //전신
-      imageRight: "https://via.placeholder.com/700x300?text=image+Side",  //두상
+      name: "<span>혼</span>다 모에",
+      imageLeft: "https://via.placeholder.com/400x700?text=400x700", //전신
+      imageRight: "https://via.placeholder.com/700x300?text=700x300",  //두상
       description: `
         내용
       `
     },
     takanashi: {
-      name: "타카나시 료마",
-      imageLeft: "https://via.placeholder.com/400x700?text=image", //전신
-      imageRight: "https://via.placeholder.com/700x300?text=image+Side",  //두상
+      name: "<span>타</span>카나시 료마",
+      imageLeft: "https://via.placeholder.com/400x700?text=400x700", //전신
+      imageRight: "https://via.placeholder.com/700x300?text=700x300",  //두상
       description: `
         내용
       `
     },
     konomi: {
-      name: "사사모리 코노미",
-      imageLeft: "https://via.placeholder.com/400x700?text=image", //전신
-      imageRight: "https://via.placeholder.com/700x300?text=image+Side",  //두상
+      name: "<span>사</span>사모리 코노미",
+      imageLeft: "https://via.placeholder.com/400x700?text=400x700", //전신
+      imageRight: "https://via.placeholder.com/700x300?text=700x300",  //두상
       description: `
         내용
       `
     },
     endo: {
-      name: "엔도 미츠키",
-      imageLeft: "https://via.placeholder.com/400x700?text=image", //전신
-      imageRight: "https://via.placeholder.com/700x300?text=image+Side",  //두상
+      name: "<span>엔</span>도 미츠키",
+      imageLeft: "https://via.placeholder.com/400x700?text=400x700", //전신
+      imageRight: "https://via.placeholder.com/700x300?text=700x300",  //두상
       description: `
         내용
       `
